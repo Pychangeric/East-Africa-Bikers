f72b3352c16b38f7dd7e09fd8674d97d12fd2b7a
pychaneric <pychangeric@gmail.com>
2023-10-27 14:31:56 +0300

    rectified the vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Specify your custom configurations here:
+  // root: 'src',
+  // base: '/my-app/',
+  // publicDir: 'public',
+  // server: {
+  //   port: 3000,
+  // },
+  // build: {
+  //   outDir: 'dist',
+  // },
 })
